164d844379876fe4407f26017a2f0d7a3cae01cc
wilvanos zhira <lex540@live.com>
2021-04-09 14:49:32 +0200

    lol

---
+++ src/components/pages/mine/MineDesk.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MineDesk extends React.Component {
+    render(){
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default MineDesk
