d8eef36367c3fc4db96da7282dcbaf43c1afe7b6
wilvanos zhira <lex540@live.com>
2021-04-09 11:53:07 +0200

    lol

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,12 +15,12 @@ function Navbars() {
                     <Link></Link>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item"><a className="nav-link" href="#">Help Desk</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#about">Admissions Desk</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#about">Assessment Portal</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#projects">Doctors Portal</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#signup">Lab Portak</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#signup">Dispensary</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Help Desk</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Admissions Desk</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Assessment Portal</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Doctors Portal</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Lab Portak</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Dispensary</a></li>
                     </ul>
                     </div>
                 </div>
