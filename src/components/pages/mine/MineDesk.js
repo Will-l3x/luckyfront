32a09f6d67fb3e5266cc1a75765284a674f5e163
wilvanos zhira <lex540@live.com>
2021-04-11 10:45:47 +0200

    wet

--- src/components/pages/mine/MineDesk.js
+++ src/components/pages/mine/MineDesk.js
@@ -1,6 +1,92 @@
 import React from 'react'
 
 class MineDesk extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            node: 'http://127.0.0.1:8000',
+
+            dataSource: [],
+            
+        }
+        this.getChain =  this.getChain.bind(this);
+        this.MineNode = this.MineNode.bind(this);
+        this.ResolveChain = this.ResolveChain.bind(this);
+        this.onHandlechangePort = this.onHandlechangePort.bind(this);
+    }
+    MineNode(){
+        fetch(this.state.node+'/mine')
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              dataSource: result,
+             
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+    getChain(){
+        
+        fetch(this.state.node+'/chain')
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              dataSource: result.chain,
+             
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+
+    }
+    ResolveChain(){
+        fetch('http://localhost:8000/nodes/resolve')
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              dataSource: result.chain,
+             
+            });
+            alert(result.message)
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+
+    }
+    onHandlechangePort(event){
+        this.setState({node: event.target.value});
+    }
     render(){
         return (
             <>
